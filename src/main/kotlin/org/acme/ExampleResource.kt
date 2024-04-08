3e092fbb0a17f2c590d64f00db268f0308bf0ee6
KYOJINNAAPYON <kyojinnaapyon@icloud.com>
2024-04-08 16:59:37 +0900

    success

---
+++ src/main/kotlin/org/acme/ExampleResource.kt
@@ -0,0 +1,30 @@
+package org.acme
+
+import io.quarkus.example.Greeter
+import io.quarkus.example.HelloRequest
+import io.quarkus.grpc.GrpcClient
+import io.smallrye.mutiny.Uni
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType
+
+@Path("/hello")
+class ExampleResource {
+
+    @GrpcClient                               
+    lateinit var hello: Greeter                            
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    fun hello(): String {
+        return "hello"
+    }
+
+    @GET
+    @Path("/{name}")
+    fun hello(name: String): Uni<String> {
+        return hello.sayHello(HelloRequest.newBuilder().setName(name).build())
+                .onItem().transform{ helloReply -> helloReply.message }
+    }
+}
